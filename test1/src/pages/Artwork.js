820f2e0b5c2cf7782c9d7d196e226697642962ca
mattraydev <cedaring.xr@gmail.com>
2022-07-09 14:18:16 -0600

    added css grid layout on artwork page

--- test1/src/pages/Artwork.js
+++ test1/src/pages/Artwork.js
@@ -6,11 +6,96 @@ import Main from '../components/Main';
 export default function Artwork() {
     return (
         <div>
-            <h1 className="page-header">VR Artwork</h1>
-            <ThreeScene />
-            <div className="break-box"></div>
-            <OpenBrushScene className="canvas-openbrush"/>
+            <div className="grid-container">
+            <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-card">
+                    <div className="grid-card-content">
+                        <div className="grid-img-container">
+                            <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                        </div>
+                        <div className="grid-card-desc-container">
+                            <h5>Artwork description</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        
     )
 }
