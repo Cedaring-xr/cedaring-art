b7a2c790fe915e49a30ef9ab4e5b83b663dff272
mattraydev <cedaring.xr@gmail.com>
2022-07-13 19:00:47 -0600

    loading artwork preview cards from json data

--- test1/src/pages/Artwork.js
+++ test1/src/pages/Artwork.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Counter from "../utils/counter";
 import ModalCard from "../components/ModalCard";
+import art from "../Assets/artwork.json";
 
  class Artwork extends Component {
     
@@ -21,42 +22,26 @@ import ModalCard from "../components/ModalCard";
         return (
             <div>
                 <div className="grid-container">
-                <div className="grid-card">
-                        <button onClick={()=> this.openModal()}>open</button>
-                        <ModalCard isOpen={this.state.isOpen}/>
-                        <div className="grid-card-content">
-                            <div className="grid-img-container">
-                                <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
+                {
+                    art.reverse().map( card => {
+                        return(
+                            <div className="grid-card">
+                                <button onClick={()=> this.openModal()}>open</button>
+                                <ModalCard isOpen={this.state.isOpen}/>
+                                <div className="grid-card-content">
+                                    <div className="grid-img-container">
+                                        <img className="grid-card-img" src={card["preview-img"]}/>
+                                    </div>
+                                    <div className="grid-card-desc-container">
+                                        <h5>{card.name}</h5>
+                                        <Counter />
+                                        <p>{card.description}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="grid-card-desc-container">
-                                <h5>Artwork description</h5>
-                                <Counter />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-card">
-                        <div className="grid-card-content">
-                            <div className="grid-img-container">
-                                <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
-                            </div>
-                            <div className="grid-card-desc-container">
-                                <h5>Artwork description</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-card">
-                        <div className="grid-card-content">
-                            <div className="grid-img-container">
-                                <img className="grid-card-img" src="/extras/clock_Moment.jpg"/>
-                            </div>
-                            <div className="grid-card-desc-container">
-                                <h5>Artwork description</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })
+                }  
                 </div>
             </div>
         )
