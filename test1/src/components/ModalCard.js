e9f2012389eb464e5468a9e8388dcf28103563fa
mattraydev <cedaring.xr@gmail.com>
2022-07-20 10:27:33 -0600

    modal and artwork page changes

--- test1/src/components/ModalCard.js
+++ test1/src/components/ModalCard.js
@@ -1,35 +1,27 @@
 import React, { useState } from "react";
+import ReactDom from 'react-dom'
 import ThreeScene from '../utils/three-scene';
 import classes from "./ModalCard.module.scss";
-import Modal from "react-modal";
 
 
+import closeIcon from "../Assets/x-png.png"
 
-// covers the artwork viewer component that should call a glb or tilt file loader scene
 
-const ModalCard = ({ isOpen, onClose }) => {
-  const [open, setOpen] = useState()
+const portalRoot = document.getElementById('portal-root')
 
-  function onClose() {
-    setOpen(isOpen = false)
-  }
-
-  if (!isOpen) {
-    console.log('modal') 
-    return null
-  } else {
-    return (
-      <div className={classes.container}>
-        <Modal isOpen={isOpen} onClose={() => isOpen(false)}>
-          <h3 className="temp-header">Modal openbrush canvas</h3>
-          <button className={classes.close} onClick={onClose}>
-            X close
-          </button>
-          <ThreeScene />
-        </Modal>
-      </div>
+export default function ModalCard({ open, onClose, children }) {
+  if (!open) return null
+  
+  return (
+    ReactDom.createPortal(
+      <div className={classes.background} onClick={onClose}>
+        <div >
+          {children}
+          <img src={closeIcon} alt="" style={{cursor: 'pointer'}} onClick={onClose} />
+          <button onClick={onClose}>test</button> 
+          {/* <ThreeScene /> */}
+        </div>
+      </div>, portalRoot
     )
-  }
+  )
 }
-
-export default ModalCard
--- test1/src/pages/Artwork.js
+++ test1/src/pages/Artwork.js
@@ -1,57 +1,33 @@
-import React, { Component } from "react";
-import Counter from "../utils/counter";
+import React, { useState } from "react";
 import ModalCard from "../components/ModalCard";
 import art from "../Assets/artwork.json";
 
-let instance = null
 
- class Artwork extends Component {
-    
-    constructor(canvas) {
-        super()
-        this.state = {
-            isOpen: false
-        }
-        if(instance) {   //singleton structure for camera import
-            return null
-        }
-        instance = this
-    }
+export default function Artwork()  {
+    const [isOpen, setOpen] = useState(false) //state starts in the parent
 
-    openModal() {
-        this.setState((prevState, props) => ({
-            isOpen: true
-        }))
-    }
-    
-    render() {
-        return (
-            <div>
-                <div className="grid-container">
-                {
-                    art.reverse().map( card => {
-                        return(
-                            <div className="grid-card">
-                                <button onClick={()=> this.openModal()}>open</button>
-                                <ModalCard isOpen={this.state.isOpen}/>
-                                <div className="grid-card-content">
-                                    <div className="grid-img-container">
-                                        <img className="grid-card-img" src={card["preview-img"]}/>
-                                    </div>
-                                    <div className="grid-card-desc-container">
-                                        <h5>{card.name}</h5>
-                                        <Counter />
-                                        <p>{card.description}</p>
-                                    </div>
+    return (
+        <div>
+            <div className="grid-container">
+                { art.reverse().map( card => {
+                    return(
+                        <div key={card.id} className="grid-card" onClick={()=> setOpen(true)}>
+                            <ModalCard  open={isOpen} onClose={() => setOpen(false)} >
+                                <h5>{card.name}</h5>
+                            </ModalCard>
+                            <div className="grid-card-content">
+                                <div className="grid-img-container">
+                                    <img className="grid-card-img" src={card["preview-img"]}/>
+                                </div>
+                                <div className="grid-card-desc-container">
+                                    <h5>{card.name}</h5>
+                                    <p>{card.description}</p>
                                 </div>
                             </div>
-                        )
-                    })
-                }  
-                </div>
+                        </div>
+                    )
+                })}  
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default Artwork;
