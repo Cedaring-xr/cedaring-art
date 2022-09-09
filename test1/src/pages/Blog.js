3f039f7b1427c8326d7b130b677a93941ff4f1b8
mattraydev <cedaring.xr@gmail.com>
2022-09-08 21:12:28 -0600

    syntax config tweaks

--- test1/src/pages/Blog.js
+++ test1/src/pages/Blog.js
@@ -1,18 +1,17 @@
-import React from "react"
-import OpenBrushScene from "../utils/OBviewer"
-import TiltLoad from "../utils/tiltLoader"
-import ShaderPractice from "../utils/shaderScene"
+import React from 'react';
+import OpenBrushScene from '../utils/OBviewer';
+import TiltLoad from '../utils/tiltLoader';
+import ShaderPractice from '../utils/shaderScene';
 
 export default function Blog() {
     return (
         <>
-            <div className='Heading_container'>
+            <div className="Heading_container">
                 <h1 className="page_header">writings</h1>
             </div>
             {/* <TiltLoad /> */}
             <OpenBrushScene />
             {/* <ShaderPractice /> */}
         </>
-        
-    )
+    );
 }
--- test1/src/pages/ReactPrac.js
+++ test1/src/pages/ReactPrac.js
@@ -1,71 +1,101 @@
-import React, { useState, useRef, useEffect } from 'react'
-import TodoList from '../components/TodoList'
-import { v4 as uuidv4 } from 'uuid'
-import { useTransition, animated } from 'react-spring'
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from '../components/TodoList';
+import { v4 as uuidv4 } from 'uuid';
+import { useTransition, animated } from 'react-spring';
 
 export default function ReactPrac() {
-  const [todos, setTodos] = useState([])
-  const [items, setItems] = useState([])
-  const todoNameRef = useRef()
-  const transition = useTransition(items, {
-    from: { x: -100, y: 800, opacity: 0 },
-    enter: item => async (next) => {
-        await next({ y: item.y, opacity: 1 , delay: item.delay}) //back to back animation chain
-        await next({ x: 0 })
-    },
-    leave: { x: 100, y: 800, opacity: 0 },
-  });
+    const [todos, setTodos] = useState([]);
+    const [items, setItems] = useState([]);
+    const todoNameRef = useRef();
+    const transition = useTransition(items, {
+        from: { x: -500, y: 350, opacity: 0 },
+        enter: (item) => async (next) => {
+            await next({ y: item.y, opacity: 1, delay: item.delay }); //back to back animation chain
+            await next({ x: -100 });
+        },
+        leave: { x: 500, y: 200, opacity: 0 }
+    });
 
-  const LOCAL_STOREAGE_KEY = 'todoApp.todos'
+    const LOCAL_STOREAGE_KEY = 'todoApp.todos';
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STOREAGE_KEY));
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
+    useEffect(() => {
+        const storedTodos = JSON.parse(
+            localStorage.getItem(LOCAL_STOREAGE_KEY)
+        );
+        if (storedTodos) setTodos(storedTodos);
+    }, []);
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STOREAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STOREAGE_KEY, JSON.stringify(todos));
+    }, [todos]);
 
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find((todo) => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
+    function toggleTodo(id) {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id === id);
+        todo.complete = !todo.complete;
+        setTodos(newTodos);
+    }
 
-  function handleAddTodo(e) {
-    console.log(todoNameRef)
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
-    })
-    todoNameRef.current.value = null
-  }
+    function handleAddTodo(e) {
+        console.log(todoNameRef);
+        const name = todoNameRef.current.value;
+        if (name === '') return;
+        setTodos((prevTodos) => {
+            return [
+                ...prevTodos,
+                { id: uuidv4(), name: name, complete: false }
+            ];
+        });
+        todoNameRef.current.value = null;
+    }
 
-  function handleClearTodos() {
-    const newTodos = todos.filter((todo) => !todo.complete)
-    setTodos(newTodos)
-  }
+    function handleClearTodos() {
+        const newTodos = todos.filter((todo) => !todo.complete);
+        setTodos(newTodos);
+    }
 
-  return (
-    <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input type="text" ref={todoNameRef} />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear Completed Todos</button>
-      <div>{todos.filter((todo) => !todo.complete).length} left todo</div>
-      <button
-        onClick={() => {
-          setItems((v) => (v.length ? [] : [{ y: -100, delay: 0 }, { y: -50, delay: 100 }, { y: 0 , delay: 200}]))
-        }}
-      >
-        {items.length ? 'un-mount' : 'mount'}
-      </button>
-      {transition((style, item) =>
-        item ? <animated.div style={style} className="spring-container" /> : ''
-      )}
-    </>
-  )
+    return (
+        <>
+            <div className="todo-container">
+                <TodoList todos={todos} toggleTodo={toggleTodo} />
+                <input type="text" ref={todoNameRef} />
+                <button onClick={handleAddTodo}>Add Todo</button>
+                <button onClick={handleClearTodos}>
+                    Clear Completed Todos
+                </button>
+                <div>
+                    {todos.filter((todo) => !todo.complete).length} left todo
+                </div>
+            </div>
+            <div className="content-block">
+                <button
+                    onClick={() => {
+                        setItems((v) =>
+                            v.length
+                                ? []
+                                : [
+                                      { y: 100, delay: 0 },
+                                      { y: 120, delay: 100 },
+                                      { y: 140, delay: 200 }
+                                  ]
+                        );
+                    }}
+                >
+                    {items.length ? 'un-mount' : 'mount'}
+                </button>
+                {transition((style, item) =>
+                    item ? (
+                        <animated.div
+                            style={style}
+                            className="spring-container"
+                        >
+                            <h1>react-spring</h1>
+                        </animated.div>
+                    ) : (
+                        ''
+                    )
+                )}
+            </div>
+        </>
+    );
 }
