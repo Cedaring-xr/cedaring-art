0f6b5c294a3190a28bb59aad3a8bf87518579afb
mattraydev <cedaring.xr@gmail.com>
2022-10-09 19:33:01 -0600

    sass cleanup and info section updates

--- test1/src/components/InfoPage.js
+++ test1/src/components/InfoPage.js
@@ -6,9 +6,15 @@ export default function InfoPage() {
     return (
         <>
             <div className={styles.container}>
+                
                 <div className={styles.text}>
                     <p className={styles.text__content}>
                         Hi! My name is Matt Ray. Welcome to my personal website. I am a web developer, VR artist, and rock climber. I built this website to showcase some of the art that I have made and also to practice my web development skills learning three JS and bridging the gap between VR and web in my own little way. I believe that the internet is the greatest learning and communication resouce that has ever existed, and I'm happy to have lived at the dawn of its existance and work in this industry.
+                    </p>
+                </div>
+                <div className={styles.text}>
+                    <p className={styles.text__content}>
+                        Hi! My name is Matt Ray. Welcome to my personal website. I am a web developer, VR artist, and rock climber. I built this website to showcase some of the art that I have made and also to practice my web development skills
                     </p>
                 </div>
                 <img src={testImage} className={styles.img} />
