cdca67ebee0ae8972c262e19c40e901265c52f23
mattraydev <cedaring.xr@gmail.com>
2022-12-04 20:23:39 -0700

    updates to climbing page

--- test1/src/pages/Climbing.js
+++ test1/src/pages/Climbing.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ScrollBasedScene from '../utils/ScrollBasedScene'
+import { BiChevronDown } from 'react-icons/bi';
 
 export default function Climbing() {
     const [view, setView] = useState(false)
@@ -11,26 +12,45 @@ export default function Climbing() {
     }
 
     return (
-        <>
+        <div className='body-content'>
             <div className="text">
                 <h1 className="tagline">Rock climbing trips</h1>
                 <p className="text__content">Working with gps and elevation data to create 3D layouts of mountains</p>
             </div>
             <div className='text climbing-text'>
-                <h3>Climbing location</h3>
-                <button onClick={() => setView(!view)} >Expand to view</button>
+                <h3>south Platte Test Demo Summit</h3>
+                <button className="view-btn" onClick={() => setView(!view)} >Click to view</button>
             </div>
             { view && <div className='canvas-container'>
+                <div className="top-text">
+                    <h2>Scroll</h2>
+                    <div className='icon'>
+                        <BiChevronDown className='chevron-down'/>
+                    </div>
+                </div>
                 <ScrollBasedScene className='scroll-scene' />
-                <section className='canvas-text'>
-                    <h4>Test section 1</h4>
-                </section>
-                <section className='canvas-text'>
-                    <h4>Test section 2</h4>
-                </section>
-                <section className='canvas-text'>
-                    <h4>Test section 3</h4>
-                </section>
+                <div className="scrol-text-container">
+                    <section className='canvas-text'>
+                        <h4>South platte, Colorado</h4>
+                        <p>Area to the south west of denver</p>
+                    </section>
+                    <section className='canvas-text'>
+                        <h4></h4>
+                        <p>starting location</p>
+                        <p>approach length: 5.5 miles round trip</p>
+                        <p>trail winds down and around then back up</p>
+                    </section>
+                    <section className='canvas-text'>
+                        <h4>Climb Stats</h4>
+                        <p>Pitches: 2 short pitches</p>
+                        <p>difficulty: easy</p>
+                    </section>
+                    <section className='canvas-text'>
+                        <h4>Summit?</h4>
+                        <p>elevation: 8224'</p>
+                        <p>other info</p>
+                    </section>
+                </div>
             </div> }
             {/* <div className='center'>
                 <div className='ring'></div>
@@ -47,6 +67,6 @@ export default function Climbing() {
                 src="../Assets/videos/clip2.mp4"
                 style={videoStyle}
                 ></video>
-        </> 
+        </div> 
     )
 }
--- test1/src/utils/ScrollBasedScene.js
+++ test1/src/utils/ScrollBasedScene.js
@@ -9,22 +9,22 @@ import * as lilGui from "lil-gui"
 gsap.registerPlugin(ScrollTrigger);
 
 class ScrollBasedScene extends Component {
+    
     componentDidMount(){
-
 
     //scene
         this.scene = new THREE.Scene();
         this.loader = new GLTFLoader()
-        this.gui = new lilGui.GUI();
+        // this.gui = new lilGui.GUI();
+        const videoPlane = new THREE.Object3D();
 
     // lights
         this.directLight = new THREE.DirectionalLight('#fff', 4)
         this.directLight.position.set(50, -50, 0)
         this.lightHelper = new THREE.DirectionalLightHelper(this.directLight, 5)
-        this.gui.add(this.directLight.position, 'x').min(-50).max(50).step(0.2).name('light-x-position')
-        this.gui.add(this.directLight.position, 'y').min(-50).max(50).step(0.2).name('light-y-position')
-        this.gui.add(this.directLight.position, 'z').min(-50).max(50).step(0.2).name('light-Z-position')
-        // this.light2 = new THREE.PointLight(0xffffff , 10)
+        // this.gui.add(this.directLight.position, 'x').min(-50).max(50).step(0.2).name('light-x-position')
+        // this.gui.add(this.directLight.position, 'y').min(-50).max(50).step(0.2).name('light-y-position')
+        // this.gui.add(this.directLight.position, 'z').min(-50).max(50).step(0.2).name('light-Z-position')
         this.light3 = new THREE.AmbientLight(0xffffff, 10)
         this.light3.position.set(50, 50, 50)
         this.scene.add(this.ligth3, this.directLight)
@@ -49,43 +49,46 @@ class ScrollBasedScene extends Component {
             side: THREE.FrontSide,
             toneMapped: false,
         })
+        videoMaterial.needsUpdate = true;
 
-        const videoGeometry = new THREE.PlaneGeometry(10, 10)
+        const videoGeometry = new THREE.PlaneGeometry(0.1, 0.077)
         let screen = new THREE.Mesh(videoGeometry, videoMaterial)
-        screen.position.set(0, 50, 0)
+        screen.position.set(2, 5, -0.6)
+        screen.rotation.set(0, 0.3,  -0.1)
         this.scene.add(screen)
 
     // model loading
+        let gpsBlock = new THREE.Object3D();
         let modelGroup = new THREE.Group();
-        let glbModel = new THREE.Object3D();
         this.loader.load('/models/noddleHead.glb', (model) => {
-            glbModel = model.scene
-            glbModel.scale.set(0.005, 0.005, 0.005)
-            glbModel.position.set(0.5, -10, -15)
-            glbModel.rotation.set(-5.88, -0.92, 0.12)
-            modelGroup.add(glbModel)
-            console.log('glb group', modelGroup)
-            this.gui.add(glbModel.position, 'z').min(-10).max(10).step(0.01).name('Z-position')
-            this.gui.add(glbModel.position, 'x').min(-10).max(10).step(0.01).name('X-position')
-            this.gui.add(glbModel.position, 'y').min(-10).max(10).step(0.01).name('y-position')
-            this.gui.add(glbModel.rotation, 'z').min(-10).max(10).step(0.01).name('z-rotation')
-            this.gui.add(glbModel.rotation, 'x').min(-10).max(10).step(0.01).name('x-rotation')
-            this.gui.add(glbModel.rotation, 'y').min(-10).max(10).step(0.01).name('y-rotation')
+            gpsBlock = model.scene
+            gpsBlock.scale.set(0.005, 0.005, 0.005)
+            gpsBlock.position.set(0, 0, 0)
+            modelGroup.add(gpsBlock)
+            // console.log('glb group', modelGroup)
+            // this.gui.add(gpsBlock.position, 'z').min(-10).max(10).step(0.01).name('Z-position')
+            // this.gui.add(gpsBlock.position, 'x').min(-10).max(10).step(0.01).name('X-position')
+            // this.gui.add(gpsBlock.position, 'y').min(-10).max(10).step(0.01).name('y-position')
+            // this.gui.add(gpsBlock.rotation, 'z').min(-10).max(10).step(0.01).name('z-rotation')
+            // this.gui.add(gpsBlock.rotation, 'x').min(-10).max(10).step(0.01).name('x-rotation')
+            // this.gui.add(gpsBlock.rotation, 'y').min(-10).max(10).step(0.01).name('y-rotation')
         })
+        let spireMtn = new THREE.Object3D();
         this.loader.load('/models/low_poly_mountain.glb', (model) => {
-            glbModel = model.scene
-            glbModel.scale.set(0.01, 0.01, 0.01)
-            glbModel.position.set(0, 0, -15)
-            glbModel.rotation.set(-5.88, -0.92, 0.12)
-            modelGroup.add(glbModel)
-            this.scene.add(modelGroup);
+            spireMtn = model.scene
+            spireMtn.scale.set(0.006, 0.006, 0.006)
+            spireMtn.position.set(-3.4, 10, -2.6)
+            // spireMtn.rotation.set(-5.88, -0.92, 0.12)
+            modelGroup.add(spireMtn)
             scrollAnimation() 
         })
 
+        this.scene.add(modelGroup);
+
     //camera
-        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1000);
-        this.camera.position.set(0, 10, 20)
-        let cameraTarget = this.cube.position
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        this.camera.position.set(20, 30, 30)
+        let cameraTarget = screen.position
         this.camera.lookAt(cameraTarget)
 
     //scroll
@@ -110,22 +113,29 @@ class ScrollBasedScene extends Component {
         const scrollAnimation = () => {
             const timeline = gsap.timeline({
                 default: {
-                    duration: 1,
+                    duration: 5,
                     ease: 'power2.inOut'
                 },
                 scrollTrigger: {
-                    trigger: ".canvas-text",
+                    trigger: ".scrol-text-container",
                     start: "top bottom",
-                    end: "bottom top",
+                    end: "bottom bottom",
                     scrub: 0.1
                 }
             })
-            timeline.to(modelGroup.rotation, {y: 5})
-            timeline.to(modelGroup.position, {z: 4, y: 2})
+            timeline.to(this.camera.position, {x: 10, y: 18, z: 20})
+            timeline.to(this.camera.position, {x: 0, y: 14, z: 5})
+            timeline.to(
+                modelGroup.rotation, {y: -2.2},
+                this.camera.position, {z: 4, y: 2}//this does something odd
+                )
+            timeline.to(this.camera.position, {x: 3, y: 13, z: 6})
+            timeline.to(this.camera.rotation, {x: 0, y: 0.3, z: -0.1})
+            timeline.to(spireMtn.position, {y: 11.8})
+            
+            timeline.to(screen.position, {x: 2, y: 13, z: -0.6})
+            timeline.to(screen.scale, {x: 40, y: 30}) //pair these two
         }
-
-    //event listeners
-        
     }
 
     animation= ()=> {
