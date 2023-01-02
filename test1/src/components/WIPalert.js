40d547834c02c080d3b2abfd5b2cc0b44df5b066
mattraydev <cedaring.xr@gmail.com>
2023-01-01 17:05:51 -0700

    quality improvements

--- test1/src/components/WIPalert.js
+++ test1/src/components/WIPalert.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import wipImage from '../Assets/images/wipArtClear.png'
 
 const WIPalert = () => {
   return (
     <div className='alert-container'>
         <h1 className='alert-message'>This is still a work in progress. Please check back later.</h1>
+        <img src={wipImage} className='wip-image'/>
     </div>
   )
 }
--- test1/src/utils/VideoScene.js
+++ test1/src/utils/VideoScene.js
@@ -7,7 +7,7 @@ export default function VideoScene({ card, onClose}) {
   return (
     <>
         <div className='video-description-box'>
-            <h2 className='video-description'>Some VR scenes are currently harder to import, so here is a video instead. I will work on getting the 3D model added in the future.</h2>
+            <h2 className='video-description'>Some VR scenes are currently harder to import due to using experimental features, so here is a video instead. I will work on getting the 3D model added in the future.</h2>
         </div>
         <video width="100%" height="100%" src={card.video} muted autoPlay loop/>
     </>
